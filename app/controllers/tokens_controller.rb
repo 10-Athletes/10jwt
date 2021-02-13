78403c484922d3e77c472da29c320f787d62489f
Jon Mendelson <mendelsonja@gmail.com>
2021-02-12 18:27:57 -0800

    starting registration

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -12,8 +12,6 @@ class TokensController < ApplicationController
 
   private
   def encode_token(payload={})
-    exp = 24.hours.from_now
-    payload[:exp] = exp.to_i
     JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 end
