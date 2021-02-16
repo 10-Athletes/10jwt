48af6855cbbbedb611a7c0127a59cec834f14052
Jon Mendelson <mendelsonja@gmail.com>
2021-02-15 18:23:21 -0800

    tweaked validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
   validates_uniqueness_of :username
-  validates_uniqueness_of :email
   validates :password,   :length => { :minimum => 8}
-
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, uniqueness: true
+  validates :email, presence: true
+  validates :username, presence: true
 end
