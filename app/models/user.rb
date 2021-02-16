a8eaa1797abdced7d0fa9a91f2722c712070d38b
Jon Mendelson <mendelsonja@gmail.com>
2021-02-15 18:38:08 -0800

    no unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :username, presence: true
-  validates :username, uniqueness: true
-  validates :password, length: { minimum: 8 }
-  validates :email, presence: true
-  validates :email, uniqueness: true
+  validates_uniqueness_of :username
+  validates :password,   :length => { :minimum => 8}
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
