408abd13c103790a42883a8b1e97e37c86e10ce9
Jon Mendelson <mendelsonja@gmail.com>
2021-02-15 18:29:31 -0800

    another try at unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_uniqueness_of :username
-  validates :password,   :length => { :minimum => 8}
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  validates :email, uniqueness: true
-  validates :email, presence: true
   validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 8 }
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
