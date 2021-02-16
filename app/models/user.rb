55e575ead7a85f02c92fbc3b2f5eaef1dd47c316
Jon Mendelson <mendelsonja@gmail.com>
2021-02-15 16:29:35 -0800

    removed email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_uniqueness_of :username, :email
+  validates_uniqueness_of :username
   validates :password,   :length => { :minimum => 8}
 
 end
