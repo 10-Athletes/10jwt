00d694679151565125549cca35518300b1f4d460
Jon Mendelson <mendelsonja@gmail.com>
2021-04-09 13:37:02 -0700

    working on report results

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_234746) do
+ActiveRecord::Schema.define(version: 2021_04_03_000330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.integer "sport"
+    t.integer "p1ID"
+    t.integer "p1InitialRating"
+    t.integer "p2ID"
+    t.integer "p2InitialRating"
+    t.integer "winner"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sports", force: :cascade do |t|
     t.string "name"
