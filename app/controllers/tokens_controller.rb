3e06ec24f21ab895d89f1a4fcf374211d4a86608
Jon Mendelson <mendelsonja@gmail.com>
2021-10-06 16:28:00 -0700

    accept email for sign in

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,6 +1,8 @@
 class TokensController < ApplicationController
   def create
     user = User.find_by('username ILIKE ?', params[:username])
+    user = User.find_by('email ILIKE ?', params[:username]) unless user
+
     if user&.authenticate(params[:password])
       render json: {
         jwt: encode_token({id: user.id, username: user.username, firstname: user.firstname, lastname: user.lastname, sports: user.sports, events: user.events})
