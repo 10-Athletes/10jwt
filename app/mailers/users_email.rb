0ce392e45ecf560517989a1b54dbb59c2ae1cb2a
Jon Mendelson <mendelsonja@gmail.com>
2021-11-26 13:58:19 -0800

    removed UsersEmail

---
+++ app/mailers/users_email.rb
@@ -0,0 +1,11 @@
+# class UsersEmail < ApplicationMailer
+#   default from: 'noreply@10athletes.com'
+#
+#   def welcome_email(user)
+#     @user = user
+#     @specialText = "Hello #{user}, welcome to 10Athletes!"
+#     mail(to: `#{user.email}`, subject: 'Welcome to 10Athletes')
+#   end
+#
+#
+# end
