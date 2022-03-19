a827b90d39a920bb7031628304b77a052b4be489
Jon Mendelson <mendelsonja@gmail.com>
2022-03-18 20:02:35 -0700

    private convert token function

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -29,9 +29,8 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
-    @user = GlobalID::Locator.locate_signed(params[:token], purpose: "password_reset")
-    if @user
-      puts @user.as_json
+    token = convertToken()
+    @user = GlobalID::Locator.locate_signed(token, purpose: "password_reset")
     end
     if @user.update(password: params[:password])
       :ok
@@ -44,5 +43,22 @@ class PasswordResetsController < ApplicationController
 
   def password_params
     params.permit(:password, :token)
+  end
+
+  #remove '%3D that was replacing equals in tokens'
+  def convertToken
+    token = ""
+    counter = 0
+    params[:token].each_char.with_index do |char, i|
+      if i < params[:token].length - 2 && char == '%' && params[:token][i+1] == '3' && params[:token][i+2] == 'D'
+        token += '='
+        counter = 2
+      elsif counter == 0
+        counter -= 1
+      else
+        token += char
+      end
+    end
+    return token
   end
 end
